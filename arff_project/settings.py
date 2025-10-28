d63603c0e9b6e1436d337baa2fb7569e048a2cee
LuzJimena050712 <luzjimenarcegabriel@gmail.com>
2025-10-27 22:21:33 -0600

    remove csrf from fetch

--- arff_project/settings.py
+++ arff_project/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#
